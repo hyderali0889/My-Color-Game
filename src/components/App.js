a97752c1de8c7ed96b58700ec7460acb884b2b75
hyderali0889 <hyderali0889@gmail.com>
2021-10-26 10:02:44 +0500

    change box count

--- src/components/App.js
+++ src/components/App.js
@@ -19,7 +19,7 @@ class Main extends React.Component {
 
 
         let newgame = () => {
-            let box_count = 4;
+            let box_count = 3;
             this.setState({ box: box_count })
             const newColor = RandomGenerator(box_count);
 
